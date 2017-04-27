dd687dc92ee79957e5dc129e171c18e061788b22
David Justice <david@devigned.com>
2017-04-27 10:31:15 -0700

    working end to end with key vault

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -40,24 +40,27 @@ else
     echo "Using the already provisioned Key Vault certificate ${cert_name}"
 fi
 
-if [[ -z "$(az ad sp show --id http://${sp_name})" ]]; then
-    echo "Generate a service principal for use by the Azure CLI in the provisioned VM to fetch connection strings"
-    service_principal=$(az ad sp create-for-rbac -n ${sp_name} --cert "$(az keyvault certificate show --vault-name ${existing_name} -n ${cert_name} --query "cer" -o tsv)")
-    echo ${service_principal}
-else
-    echo "Using the following service principal:"
-    az ad sp show --id "http://${sp_name}"
+if [[ ! -z "$(az ad sp show --id http://${sp_name})" ]]; then
+    az ad app delete --id "http://${sp_name}" 1>/dev/null
 fi
+
+echo "Generate a service principal for use by the Azure CLI in the provisioned VM to fetch connection strings"
+rm -f "${cert_name}.pem"
+az keyvault certificate download --vault-name ${existing_name} -n ${cert_name} -f "${cert_name}.pem" 1>/dev/null
+service_principal=$(az ad sp create-for-rbac -n ${sp_name} --cert "@${cert_name}.pem")
+echo ${service_principal}
 
 if [[ "$(az vm show -g ${group_id} -n ${vm_name} --query "provisioningState=='Success'")" == 'false' ]]; then
     secret=$(az keyvault secret list-versions --vault-name ${existing_name} -n ${cert_name} --query "[?attributes.enabled].id" -o tsv)
     vm_secret=$(az vm format-secret -s "${secret}")
     echo "Creating VM with provisioned Key Vault secret (certificate use for service principal auth)"
-    az vm create -g ${group_id} -n ${vm_name} --admin-username deploy --image debian --secrets "$vm_secret" --custom-data "${DIR}/cloud-config.yml" 1>/dev/null
+    az vm create -g ${group_id} -n ${vm_name} --admin-username deploy --image UbuntuLTS --secrets "$vm_secret" --custom-data "${DIR}/cloud-config.yml" 1>/dev/null
     az vm open-port -g ${group_id} -n ${vm_name} --port 80 1>/dev/null
 else
     echo "Using existing VM ${vm_name} in group ${group_id}, which has already been provisioned with a Key Vault secret (certificate use for service principal auth)"
 fi
+
+echo "SSH into the VM: ssh deploy@$(az vm show -d -g level1 -n level1-vm --query 'publicIps' -o tsv)"
 
 
 
