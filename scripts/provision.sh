628aa0ad42812c45c48d30f9dc5dfd99dfc27795
David Justice <david@devigned.com>
2017-04-27 15:45:12 -0700

    provisions a functioning az-cli authed machine

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -47,18 +47,21 @@ fi
 echo "Generate a service principal for use by the Azure CLI in the provisioned VM to fetch connection strings"
 rm -f "${cert_name}.pem"
 az keyvault certificate download --vault-name ${existing_name} -n ${cert_name} -f "${cert_name}.pem" 1>/dev/null
-service_principal=$(az ad sp create-for-rbac -n ${sp_name} --cert "@${cert_name}.pem")
-echo ${service_principal}
+az ad sp create-for-rbac -n ${sp_name} --cert "@${cert_name}.pem"  1>/dev/null
 
 if [[ "$(az vm show -g ${group_id} -n ${vm_name} --query "provisioningState=='Success'")" == 'false' ]]; then
     secret=$(az keyvault secret list-versions --vault-name ${existing_name} -n ${cert_name} --query "[?attributes.enabled].id" -o tsv)
     vm_secret=$(az vm format-secret -s "${secret}")
     echo "Creating VM with provisioned Key Vault secret (certificate use for service principal auth)"
+    tenant="$(az account show --query 'tenantId' -o tsv)"
+    fingerprint="$(az keyvault certificate show --vault-name ${existing_name} -n ${cert_name} --query "x509ThumbprintHex" -o tsv)"
+    sed -- "s/{{ tenant }}/${tenant}/g;s/{{ username }}/http:\/\/${sp_name}/g;s/{{ fingerprint }}/${fingerprint}/g" scripts/cloud-config-template.yml > "${DIR}/cloud-config.yml"
     az vm create -g ${group_id} -n ${vm_name} --admin-username deploy --image UbuntuLTS --secrets "$vm_secret" --custom-data "${DIR}/cloud-config.yml" 1>/dev/null
     az vm open-port -g ${group_id} -n ${vm_name} --port 80 1>/dev/null
 else
     echo "Using existing VM ${vm_name} in group ${group_id}, which has already been provisioned with a Key Vault secret (certificate use for service principal auth)"
 fi
+
 
 echo "SSH into the VM: ssh deploy@$(az vm show -d -g level1 -n level1-vm --query 'publicIps' -o tsv)"
 
