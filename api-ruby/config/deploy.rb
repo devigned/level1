b4b63a374811693514f18411bbabaa85f29061b7
David Justice <david@devigned.com>
2017-04-28 15:41:10 -0700

    fix puma bind

--- api-ruby/config/deploy.rb
+++ api-ruby/config/deploy.rb
@@ -20,8 +20,8 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/api-ruby/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/api-ruby/log/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -55,6 +55,7 @@ namespace :deploy do
   task :initial do
     on roles(:app) do
       invoke 'deploy'
+      execute "mkdir #{release_path}/log -p"
       execute "sudo rm -f /etc/nginx/sites-enabled/default"
       execute "sudo ln -nfs #{release_path}/config/nginx.conf /etc/nginx/sites-enabled/level1"
       execute "sudo service nginx restart"
