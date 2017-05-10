3c761463102d0a61e623e1a96702b25d27d75e41
David Justice <david@devigned.com>
2017-05-09 19:09:53 -0700

    deploys and provisions cleanly

--- api-ruby/config/deploy.rb
+++ api-ruby/config/deploy.rb
@@ -20,8 +20,8 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/api-ruby/log/puma.access.log"
-set :puma_error_log,  "#{release_path}/api-ruby/log/puma.error.log"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -33,8 +33,7 @@ namespace :puma do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
-      execute "mkdir #{current_path}/api-ruby/log -p"
-      execute "mkdir #{current_path}/log -p"       
+      execute "mkdir #{release_path}/log -p"       
     end
   end
 
@@ -58,11 +57,12 @@ namespace :deploy do
     on roles(:app) do
       api_host = `az network public-ip show -g level1 -n level1-vmss --query 'dnsSettings.fqdn' -o tsv`.strip
       cmd = <<-CMD
-      cd '#{release_path}/todo-ember'&& npm install --no-progress && \
-      sed -- "s/{{ API_HOST }}/http:\/\/#{api_host}/g" config/environment.js -i
+      cd '#{release_path}/todo-ember' &&  \
+      npm install --no-progress > log.txt 2>&1 && \
+      sed -i 's/{{ API_HOST }}/http:\\/\\/#{api_host}/g' config/environment.js && \
       CDN_HOST=http://$(az cdn endpoint list -g level1 --profile-name level1 --query '[0] | hostName' -o tsv)/ ./node_modules/ember-cli/bin/ember build --silent --environment production && \
       rm -rf ../public/* && \
-       cp -R dist/* ../public
+      cp -R dist/* ../public
 CMD
        execute cmd
     end
